0f2f11f593aba7daf47f99c4e9bb322e8506e23c
bilza2023 <bilza2023@gmail.com>
2026-01-09 19:42:56 +0500

    pause before background 4 slides complete

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -1,39 +1,43 @@
-// import { slideBuilder } from "../../node_modules/taleem-slides/src/index.js";
 import { slideBuilder } from "taleem-slides";
-
 import { loadDeck } from "./loadDeck.js";
 
 const root = document.getElementById("app");
 
 let manager;
 let currentIndex = 0;
+let totalSlides = 0;
 
 function render() {
   root.innerHTML = manager.renderSlide(currentIndex);
 }
 
 async function init() {
-  const deck = await loadDeck("/decks/goldstandar_eq_28aug25.json");
+  const deck = await loadDeck("/decks/demo-gold.json");
 
-  // 🔒 interpret ONCE
+  // interpret ONCE
   manager = slideBuilder(deck);
+  totalSlides = deck.deck.length;
 
   render();
 
   document.addEventListener("keydown", (e) => {
     if (e.key === "ArrowRight") {
-      currentIndex++;
-      render();
+      if (currentIndex < totalSlides - 1) {
+        currentIndex++;
+        render();
+      }
     }
 
-    if (e.key === "ArrowLeft" && currentIndex > 0) {
-      currentIndex--;
-      render();
+    if (e.key === "ArrowLeft") {
+      if (currentIndex > 0) {
+        currentIndex--;
+        render();
+      }
     }
   });
 }
 
-init().catch(err => {
+init().catch((err) => {
   console.error(err);
   root.innerHTML = `<pre style="color:red">${err.message}</pre>`;
 });
